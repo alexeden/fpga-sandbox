ec9dfd58061c67b1e908119cb94e37bd2979e30e
Alex Eden <alexandereden91@gmail.com>
2019-01-27 01:58:24 -0600

    Consolidate the tflipflop and clockdivider sources

--- sw2hex/sw2hex.srcs/sources_1/new/clockDivider.v
+++ sw2hex/sw2hex.srcs/sources_1/new/clockDivider.v
@@ -1,16 +1,17 @@
 `timescale 1ns / 1ps
 
 module clockDivider(
-		input clockIn,
-		output clockOut
-	);
+	input  clockIn,
+	output clockOut
+);
+
 	parameter clockdivs = 1;
 
-
 	wire[clockdivs:0] c;
+    
     assign c[0] = clockIn;
     assign clockOut = c[clockdivs];
-     genvar i;
+    genvar i;
 
     generate
         for(i = 0; i <= clockdivs - 1; i = i+1)
@@ -23,6 +24,30 @@ module clockDivider(
     	$display("display driver clockDivider has %d divisions", clockdivs);
     end
 
+endmodule
 
- 
+
+module tflipflop(
+    input Clock,
+	input Reset,	
+	input En,
+	output reg Q
+);
+
+	wire xorTtoD;
+
+	initial Q = 1'b0;
+
+	xor 
+		g(xorTtoD, Q, En);
+
+	always @(negedge Reset, posedge Clock)
+	begin
+		// Reset occurs when low.
+		if(!Reset)
+			Q <= 0;
+		else
+			Q <= xorTtoD;
+	end
+
 endmodule
--- sw2hex/sw2hex.srcs/sources_1/new/displayDriver.v
+++ sw2hex/sw2hex.srcs/sources_1/new/displayDriver.v
@@ -3,19 +3,16 @@
 // Controls the 8, seven-segment displays.
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module displayDriver(
-	input Clock,
-    input [31:0] value,
-    output [7:0] NOTan, 		// Digit enable (select)
-    output [6:0] seg
+	input   Clock,
+    input   [31:0] value,
+    output  [7:0] NOTan, 		// Digit enable (select)
+    output  [6:0] seg
 );
 
 	wire [7:0] an;
 
 	assign NOTan = ~an;
-
-
 
 	// Create the 3-bit (max 8) control signal.
 	// Controls value demuxing of 32-bit display value to 4-bit subvalues.
@@ -35,7 +32,7 @@ module displayDriver(
 	// Create a slowed down Clock signal.
 	wire dividedclock;
 
-	// Bring the Clock freque8ncy down to a level low enough for display.
+	// Bring the Clock frequecncy down to a level low enough for display.
 	defparam cd.clockdivs = 12;
 	clockDivider cd(Clock, dividedclock);
 
@@ -45,9 +42,10 @@ module displayDriver(
     	$display("Divided clock pulsed at ", $time, " control value is %d", control);
     	if(control != digits - 1)
     		control = control + 1;
-    	else begin
-    			control = 0;
-    	end
+    	else 
+    	   begin
+    		  control = 0;
+    	   end
     end
 
     // Initialize the 32-bit signal demultiplexer.
@@ -59,17 +57,17 @@ module displayDriver(
     //Decode the control counter to the digit enable.
     decoder3to8 dec38(control, an);
 
-
 endmodule
 
-module valueDemux(
-		input [2:0] s,
-		input [31:0] fullvalue,
-		output reg [3:0] subvalue
-	);
-	// Receives the 32 value bits. The 3-bit switch value controls
-	// which 4-bit section of the value is sent to the segment translator.
 
+// Receives the 32 value bits. The 3-bit switch value controls
+// which 4-bit section of the value is sent to the segment translator.
+module valueDemux(
+    input [2:0] s,
+	input [31:0] fullvalue,
+	output reg [3:0] subvalue
+);
+	
 	always @(*)
 	begin
 		case (s)
@@ -87,56 +85,111 @@ module valueDemux(
 endmodule
 
 
+// Receives the 4-bit digit to be displayed and translates it to the segment code.
 module segmentEncoder(
-		input [3:0] digitvalue,
-		output reg [6:0] segmentcode
-	);
+    input      [3:0] digitvalue,
+	output reg [6:0] segmentcode
+);
 
-	// Receives the 4-bit digit to be displayed and translates it to the segment code.
 	always @(digitvalue)
-		begin
-			case(digitvalue)
-				0: segmentcode = 7'b1000000;
-				1: segmentcode = 7'b1111001;
-				2: segmentcode = 7'b0100100; 
-				3: segmentcode = 7'b0110000;//0000110;
-				4: segmentcode = 7'b0011001;//1001100;
-				5: segmentcode = 7'b0010010;//0100100;
-				6: segmentcode = 7'b0000010;//0100000;
-				7: segmentcode = 7'b1111000;//0001111;
-				8: segmentcode = 7'b0000000;
-				9: segmentcode = 7'b0010000;//0000100;
-				'hA: segmentcode = 7'b0001000;
-				'hb: segmentcode = 7'b0000011;//1100000;
-				'hC: segmentcode = 7'b1000110;//0110001;
-				'hd: segmentcode = 7'b0100001;//1000010;
-				'hE: segmentcode = 7'b0000110;//0110000;
-				'hF: segmentcode = 7'b0001110;//0111000;
-				default: segmentcode = 7'b0000000; // 0
-			endcase 
-		end
+	begin
+		case(digitvalue)
+			'h0:    segmentcode = 7'b1000000;
+			'h1:    segmentcode = 7'b1111001;
+			'h2:    segmentcode = 7'b0100100; 
+			'h3:    segmentcode = 7'b0110000;
+			'h4:    segmentcode = 7'b0011001;
+			'h5:    segmentcode = 7'b0010010;
+			'h6:    segmentcode = 7'b0000010;
+			'h7:    segmentcode = 7'b1111000;
+			'h8:    segmentcode = 7'b0000000;
+			'h9:    segmentcode = 7'b0010000;
+			'hA:    segmentcode = 7'b0001000;
+			'hb:    segmentcode = 7'b0000011;
+			'hC:    segmentcode = 7'b1000110;
+			'hd:    segmentcode = 7'b0100001;
+			'hE:    segmentcode = 7'b0000110;
+			'hF:    segmentcode = 7'b0001110;
+			default: 
+			        segmentcode = 7'b0000000; 
+		endcase 
+	end
 
 endmodule
 
 
 module decoder3to8(
-		input [2:0] i,
-		output [7:0] d
-	);
+	input  [2:0] i,
+	output [7:0] d
+);
 
 	// Decodes the 3-bit control signal.
 	wire [2:0] ni;
+	
 	not 
 		n2(ni[2], i[2]),
 		n1(ni[1], i[1]),
 		n0(ni[0], i[0]);
 	and
-		a7(d[7], i[2], i[1], i[0]),
-		a6(d[6], i[2], i[1], ni[0]),
-		a5(d[5], i[2], ni[1], i[0]),
-		a4(d[4], i[2], ni[1], ni[0]),
-		a3(d[3], ni[2], i[1], i[0]),
-		a2(d[2], ni[2], i[1], ni[0]),
-		a1(d[1], ni[2], ni[1], i[0]),
-		a0(d[0], ni[2], ni[1], ni[0]);
+		a7(d[7], i[2],    i[1],   i[0]),
+		a6(d[6], i[2],    i[1],   ni[0]),
+		a5(d[5], i[2],    ni[1],  i[0]),
+		a4(d[4], i[2],    ni[1],  ni[0]),
+		a3(d[3], ni[2],   i[1],   i[0]),
+		a2(d[2], ni[2],   i[1],   ni[0]),
+		a1(d[1], ni[2],   ni[1],  i[0]),
+		a0(d[0], ni[2],   ni[1],  ni[0]);
+endmodule
+
+
+module clockDivider(
+	input  clockIn,
+	output clockOut
+);
+
+	parameter clockdivs = 1;
+
+	wire[clockdivs:0] c;
+    
+    assign c[0] = clockIn;
+    assign clockOut = c[clockdivs];
+    genvar i;
+
+    generate
+        for(i = 0; i <= clockdivs - 1; i = i+1)
+            begin:dividers
+                tflipflop d(c[i], 1'b1, 1'b1, c[i+1]);
+        end
+    endgenerate
+
+    initial begin
+    	$display("display driver clockDivider has %d divisions", clockdivs);
+    end
+
+endmodule
+
+
+module tflipflop(
+    input Clock,
+	input Reset,	
+	input En,
+	output reg Q
+);
+
+	wire xorTtoD;
+
+	initial Q = 1'b0;
+
+	xor 
+		g(xorTtoD, Q, En);
+
+	always @(negedge Reset, posedge Clock)
+	begin
+		// Reset occurs when low.
+		if(!Reset)
+			Q <= 0;
+		else
+			Q <= xorTtoD;
+	end
+
 endmodule
